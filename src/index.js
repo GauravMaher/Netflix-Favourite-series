9c16d6655a3cd4b760f668c5258f2405f97d50e9
Gaurav Maher <gauravmaher2152003@gmail.com>
2022-01-03 21:43:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './Card';
 import "./index.css";
-import Sdata from './Sdata';
+import App from './App';
 
 
-// console.log(Sdata[0]);
-
-// function ncard(val){
-//     console.log(val);
-// }
-
-
-ReactDOM.render(
-    <>
-
-        <h1 className='heading_style'>List of top 5 Netflix series in 2020</h1>
-         { Sdata.map((val, index) => {
-             console.log(index);
-            return(
-        <Card imgsrc={val.imgsrc}
-            title={val.title}
-            sname={val.sname}
-            link={val.link}
-        />
-            );
-        } )}
-    </>
-    , document.getElementById('root')
-);
+ReactDOM.render(<App/> , document.getElementById('root'));
